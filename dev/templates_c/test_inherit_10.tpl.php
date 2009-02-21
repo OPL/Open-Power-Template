0f5f072e219c6fb1e18445476a2c32e2e1c35017
eXtreme <jacek.jedrzejewski@gmail.com>
2009-02-21 11:34:51 +0000

    * [OPT20] Markdown syntax fixes in docs * [OPT20] svn:ignore once again

--- dev/templates_c/test_inherit_10.tpl.php
+++
@@ -1,6 +0,0 @@
-<?php if(!$this->_massPreprocess($this->_template, $compileTime, array('test_inherited_d.tpl',))){   ?><<?php echo '?'; ?>xml version="1.0" standalone="yes" ?>
-
-<p>Hi</p>
-
-		<p>Foo</p>
-	<?php  }else{ $compileTime = $this->_compile($this->_template, $mode); require(__FILE__); }   ?>
