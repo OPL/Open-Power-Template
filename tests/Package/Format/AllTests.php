b9ac3d64a9c0b860142e506609e034a3d1ded4a4
zyxist <nightstorm@tlen.pl>
2010-08-30 15:59:51 +0200

    Adding test launcher for data formats.

---
+++ tests/Package/Format/AllTests.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * The test suite file that configures the execution of the test cases.
+ *
+ * @author Tomasz "Zyx" Jędrzejewski
+ * @copyright Copyright (c) 2009 Invenzzia Group
+ * @license http://www.invenzzia.org/license/new-bsd New BSD License
+ */
+
+class Package_AllTests extends PHPUnit_Framework_TestSuite
+{
+
+	/**
+	 * Configures the suite object.
+	 *
+	 * @return Suite
+	 */
+	public static function suite()
+	{
+		$suite = new Package_AllTests('Package_Format');
+	//	$suite->addTestSuite('Package_Format_ObjectiveTest');
+	//	$suite->addTestSuite('Package_Format_SplDatastructureTest');
+	//	$suite->addTestSuite('Package_Format_StaticGeneratorTest');
+
+		return $suite;
+	} // end suite();
+
+	/**
+	 * Configures the OPL autoloader and installs the libraries.
+	 */
+	protected function setUp()
+	{
+		/* currently null */
+	} // end setUp();
+
+	/**
+	 * Shuts down the test procedure.
+	 */
+	protected function tearDown()
+	{
+		/* currently null */
+	} // end tearDown();
+} // end Package_Format_AllTests;
