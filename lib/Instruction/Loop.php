d8ae357a8f95d9dcc8c21e6116c8cb660cc7aba6
zyxist <nightstorm@tlen.pl>
2010-10-09 09:07:35 +0000

    Adding a modification similar to that from OPT 2.1 that allows opt:separator to work with attribute forms.

--- lib/Instruction/Loop.php
+++ lib/Instruction/Loop.php
@@ -19,6 +19,7 @@
 	 */
 	abstract class Opt_Instruction_Loop extends Opt_Compiler_Processor
 	{
+		const ATTRIBUTE_FORM = true;
 
 		/**
 		 * Adds the "separator" tag and attribute support to the loop instructions.
@@ -29,8 +30,9 @@
 		 * @param String $varname The variable name used by the separator PHP code to detect the first element
 		 * @param String $arg If this argument is not NULL, it should contain the value from the "separator" attribute in the instruction tag.
 		 * @param Opt_Xml_Scannable $node The loop node.
+		 * @param boolean $attributeForm Optimize for the attribute form?
 		 */
-		public function processSeparator($varname, $arg, Opt_Xml_Scannable $node)
+		public function processSeparator($varname, $arg, Opt_Xml_Scannable $node, $attributeForm = false)
 		{
 			$items = $node->getElementsByTagNameNS('opt', 'separator', false);
 			
@@ -38,23 +40,47 @@
 			{
 				case 1:
 					// Move this node to the beginning
-					$node->removeChild($items[0]);				
-					$node->insertBefore($items[0], 0);
-					$this->_process($items[0]);
-					$items[0]->set('hidden', false);
+					if($attributeForm)
+					{
+						$node->removeChild($items[0]);
+						$node->getParent()->insertBefore($items[0], $node);
+						$items[0]->set('hidden', false);
+						$this->_process($items[0]);
+						$items[0]->copyBuffer($node, Opt_Xml_Buffer::TAG_BEFORE, Opt_Xml_Buffer::TAG_BEFORE);
+						$node->clear(Opt_Xml_Buffer::TAG_BEFORE);
 
-					// Add PHP code
-					$node->addBefore(Opt_Xml_Buffer::TAG_BEFORE, ' '.$varname.' = 0;');
-					$items[0]->addBefore(Opt_Xml_Buffer::TAG_BEFORE, 'if('.$varname.' == 1){');
-					$items[0]->addAfter(Opt_Xml_Buffer::TAG_AFTER, '}else{ '.$varname.' = 1; }');
-					break;
+						// Add PHP code
+						$items[0]->addBefore(Opt_Xml_Buffer::TAG_BEFORE, ' '.$varname.' = 0;');
+						$items[0]->addBefore(Opt_Xml_Buffer::TAG_CONTENT_BEFORE, 'if('.$varname.' == 1){');
+						$items[0]->addAfter(Opt_Xml_Buffer::TAG_CONTENT_AFTER, '}else{ '.$varname.' = 1; }');
+					}
+					else
+					{
+						$node->removeChild($items[0]);
+						$node->insertBefore($items[0], 0);
+						$this->_process($items[0]);
+						$items[0]->set('hidden', false);
+
+						// Add PHP code
+						$node->addBefore(Opt_Xml_Buffer::TAG_BEFORE, ' '.$varname.' = 0;');
+						$items[0]->addBefore(Opt_Xml_Buffer::TAG_BEFORE, 'if('.$varname.' == 1){');
+						$items[0]->addAfter(Opt_Xml_Buffer::TAG_AFTER, '}else{ '.$varname.' = 1; }');
+					}
+					return $items[0];
 				case 0:
-					if(!is_null($arg))
+					if($arg !== null)
 					{
 						$node->addBefore(Opt_Xml_Buffer::TAG_BEFORE, $varname.' = 0;');
-						$node->addBefore(Opt_Xml_Buffer::TAG_CONTENT_BEFORE, 'if('.$varname.' == 1){ echo '.$arg.'; }else{ '.$varname.' = 1; }');
+						if($attributeForm)
+						{
+							$node->addAfter(Opt_Xml_Buffer::TAG_OPENING_BEFORE, 'if('.$varname.' == 1){ echo '.$arg.'; }else{ '.$varname.' = 1; }');
+						}
+						else
+						{
+							$node->addBefore(Opt_Xml_Buffer::TAG_CONTENT_BEFORE, 'if('.$varname.' == 1){ echo '.$arg.'; }else{ '.$varname.' = 1; }');
+						}
 					}
-					break;
+					return null;
 				default:
 					throw new Opt_InstructionTooManyItems_Exception('opt:separator', $node->getXmlName(), 'Zero or one');
 			}
